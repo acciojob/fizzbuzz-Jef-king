261eae9281ab33ff9394a6a061459af7663db2c3
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-07-19 22:39:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let s='';
+for(int i=1;i<21;i++){
+	let x=(i%3)?true:false,y=(i%5)?true;false;
+	if(x && y){
+		s+="FizzBuzz";
+	}else if(x){
+		s+="Fizz";
+	else if(y){
+		s+="Buzz";
+	}else{
+		s+=i;
+	}
+}
+alert(s);
